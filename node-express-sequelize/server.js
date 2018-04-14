a52bd91f8887f479a89494c4d730949a2caf5354
sarakoki <sarakoki94@gmail.com>
2018-04-14 09:39:37 +0300

    ..

--- node-express-sequelize/server.js
+++ node-express-sequelize/server.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var app = express();
 var Sequelize = require('sequelize');
+var router = express.router()
+var wiki = require('./wiki.js')
 var sequelize = new Sequelize('test', 'test', null, { dialect: 'sqlite', storage: './db.test.sqlite' });
+
 
 var User = sequelize.define('User', {
   username: Sequelize.STRING
@@ -10,9 +13,17 @@ var User = sequelize.define('User', {
 /*  Create a '/users' route that responds to 
     a GET request with all users in the database */
 
+//
+app.use('wiki',wiki);
+
+app.router.get('/users', function(req,res) {
+	console.log('users')
+	res.send(users);
+});
 
 
 module.exports = { 
   app: app,
-  User: User
+  User: User,
+  router: router
 };
